3d361b12f024bbe85bb3b338bd4b105e7b5b46be
Yubaraj <yuvrajwebdev@gmail.com>
2022-08-20 04:58:32 +0530

    Most of the designing is completed. It's deployable. Haven't included the animations yet.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Nav from './components/Nav';
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About'
-// import Dumplings from './components/Dumplings'
+import Dumplings from './components/Dumplings'
 import Contact from './components/Contact'
 import Home from './components/Home'
 
@@ -12,9 +12,9 @@ function App() {
       <Nav />
       <Routes>
         <Route exact path='/' element={<Home />} />
-        {/* <Route exact path="/" element={<Home />} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/divine-dumplings" element={<Dumplings />} />
       </Routes>
     </div>
   );
