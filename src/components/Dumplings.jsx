496c4be2f9637c096fe2960299b39ae357451e75
Yubaraj <yuvrajwebdev@gmail.com>
2022-08-19 18:34:15 +0530

    first commit

---
+++ src/components/Dumplings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dumplings() {
+  return (
+    <div>
+      <h1>This is Divine Dumplings</h1>
+    </div>
+  )
+}
+
+export default Dumplings
---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <span>DIVINE</span>
+      <ul>
+        <li>
+          <a href="/">Dumplings</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
